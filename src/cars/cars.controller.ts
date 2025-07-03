e63e4fc0560e1cdce1a494716fddcf53f2e062bd
José <jcuencacoronado@gmail.com>
2025-07-02 23:42:19 -0400

    Endpoints POST y PATCH para car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Param, ParseIntPipe, Body } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -17,4 +17,14 @@ export class CarsController {
         console.log({id});
         return this.CarsService.findOneById(id);
     }
+
+    @Post()
+    createCar(@Body() body: any) {
+        return body;
+    }
+    @Patch(':id')
+    updateCar (@Body() body: any) {
+        return body;
+    }
+    
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -6,23 +6,17 @@ export class CarsService {
         {
             id: 1,
             make: 'Toyota',
-            model: 'Corolla',
-            year: 2020,
-            price: 20000
+            model: 'Corolla'
         },
         {
             id: 2,
             make: 'Honda',
-            model: 'Civic',
-            year: 2021,
-            price: 22000
+            model: 'Civic'
         },
         {
             id: 3,
             make: 'Ford',
-            model: 'Focus',
-            year: 2019,
-            price: 18000
+            model: 'Focus'
         }
     ];
     findAll() {
@@ -30,9 +24,8 @@ export class CarsService {
     }
     findOneById(id: number) {
         const car = this.cars.find(car => car.id === id);
-        if (!car) {
+        if (!car) 
             throw new NotFoundException(`Car with ID ${id} not found`);
-        }
         return car;
     }
 }
