0d42634346cb25f28dde24b9f84c42893ea82651
José <jcuencacoronado@gmail.com>
2025-07-02 22:05:15 -0400

    Creacion endpoints get para carros y carro por id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+    private cars = ['Toyota', 'Honda', 'Ford'];
+    
+    @Get()
+    getAllCars() {
+        return this.cars;
+    }
+    
+    @Get(':id')
+    getCarById(@Param('id') id: string) {
+        console.log(`Fetching car with ID: ${id}`);
+        return this.cars[id];
+    }
+}
