2b1cc702cf507f0313b226472cbadb93292722ce
José <jcuencacoronado@gmail.com>
2025-07-02 23:49:19 -0400

    Endpoint DELETE para car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Param, ParseIntPipe, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, ParseIntPipe, Body } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -25,6 +25,13 @@ export class CarsController {
     @Patch(':id')
     updateCar (@Body() body: any) {
         return body;
+    }
+    @Delete(':id')
+    deleteCar (@Param('id', ParseIntPipe) id: number) {
+        return {
+            method: 'delete',
+            id,
+        };
     }
     
 }
